fd81824a772097753c00d3b232c88684a8b33eeb
Qidwai <samiqidwai@gmail.com>
2021-09-07 20:06:44 +0500

    my first comment

---
+++ pages/hello/index.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const Cars = ()=>{
+ 
+  return (
+    <>
+      <nav style={{background:"red"}}> </nav>
+      <nav style={{background:"yellow"}}> </nav>
+    </>
+  );
+}
+
+export default Cars;
