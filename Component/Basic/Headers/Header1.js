c75b6e8c61e39b8893e41da5180cd0359e04a844
Sameer Ahmed Qidwai <samiqidwai@gmail.com>
2023-03-27 13:43:25 +0500

    1st header

--- Component/Basic/Headers/Header1.js
+++ Component/Basic/Headers/Header1.js
@@ -1,69 +1,59 @@
 import React from 'react'
 import { Col, Row, Button, Tooltip, Typography, Layout } from 'antd'
-import { FacebookOutlined, TwitterOutlined, InstagramOutlined, RedditOutlined, LinkedinOutlined} from '@ant-design/icons';
+import { FacebookFilled , TwitterSquareFilled , InstagramFilled , RedditOutlined, LinkedinOutlined} from '@ant-design/icons';
 
-const { Paragraph } = Typography;
+const { Paragraph, Text } = Typography;
 const { Header } = Layout
 const Header1 = (props) =>{
     return (
       <Header style={styles.header}>
         <div>
-          <Row  className="header1 px-30 py-2" justify="space-between" align="middle">
-            <Col className='pt-10' xs={24} sm={10}>
-              <Row gutter={[16]} className="xs-center" >
+          <Row className="hedear-basement-1" align="middle">
+            <Col  xs={24} sm={8} md={8}>
+              <a href="https://mylegalbasement.ca/">
+                <img className="header_logo" src="https://mylegalbasement.ca/wp-content/themes/mylegalbasement/images/logo.png" alt=""/>
+              </a>
+            </Col>
+            <Col xs={24} sm={8} md={10}>
+              <Row gutter={[16]} justify="end" align="middle">
                 <Col>
                   <Tooltip title="Facebook" arrowPointAtCenter>
                     <a target={"_blank"} href="https://www.facebook.com/" className='reset-a'> 
-                      <FacebookOutlined className="fs-18"/> 
+                      <FacebookFilled  className="fs-25 fb-icon"/> 
                     </a> 
                   </Tooltip>
                 </Col>
                 <Col>
                   <Tooltip title="Twitter" arrowPointAtCenter>
                     <a target={"_blank"} href="https://www.twitter.com/" className='reset-a'> 
-                      <TwitterOutlined className="fs-18"/> 
-                    </a> 
-                  </Tooltip>
-                </Col>
-                <Col>
-                  <Tooltip title="LinkedIn" arrowPointAtCenter>
-                    <a target={"_blank"} href="https://www.linkedIn.com/" className='reset-a'> 
-                      <LinkedinOutlined className="fs-18"/> 
+                      <TwitterSquareFilled  className="fs-25 tw-icon"/> 
                     </a> 
                   </Tooltip>
                 </Col>
                 <Col>
                   <Tooltip title="Instagram" arrowPointAtCenter>
                     <a target={"_blank"} href="https://www.instagram.com/" className='reset-a'> 
-                      <InstagramOutlined className="fs-18"/> 
+                      {/* <InstagramFilled  className="fs-25 ig-icon"/>  */}
+                      <div class="instagram"></div>
                     </a> 
                   </Tooltip>
                 </Col>
               </Row>
+
             </Col>
-            <Col className="header-1-right" >
-              <Row gutter={[20]} align="middle" className="xs-center" >
-                <Col>
-                  <Tooltip title="Goto" arrowPointAtCenter>
-                    <a target={"_blank"} href="https://www.Blogspot.com/" className='reset-a'> 
-                      <span className='fs-13'>Blog</span> 
-                    </a> 
-                  </Tooltip>
-                </Col>
-                <Col  >
-                  <Tooltip title="Contact Now" arrowPointAtCenter>
-                      <Button  
-                        type="primary" 
-                        size='large' 
-                        block 
-                        href="tel:+16472189786"
-                        className='bg-33f border-0'
-                      >
-                        <span className='fs-14'>Call Us +1 (647) 218-9786</span>
-                      </Button>
-                  </Tooltip>
-                </Col>
-              </Row>
+
+            <Col xs={24} sm={8} md={6}>
+              <Button 
+                type="primary" 
+                size='large'
+                className="header-basment-btn"
+                block 
+                href="tel:+16472189786"
+              >
+                <span className='fs-13 lh-1'>we are only a phone call away</span>
+                <br/>
+                <span> 647 218 9786</span>
+              </Button>
             </Col>
           </Row>
         </div>
