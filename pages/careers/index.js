ea809ce9d26072f010c027066d7c9e0ae08ae306
Sameer Ahmed Qidwai <samiqidwai@gmail.com>
2023-04-01 17:06:56 +0500

    pushing

--- pages/careers/index.js
+++ pages/careers/index.js
@@ -1,41 +1,57 @@
 import React from 'react';
-import { Col, Row, Typography, Button, Divider, Form, Input, Upload } from 'antd';
+import {
+  Col,
+  Row,
+  Typography,
+  Button,
+  Divider,
+  Form,
+  Input,
+  Upload,
+} from 'antd';
 import { useRouter } from 'next/router';
 import { UploadOutlined } from '@ant-design/icons';
 
 const { Title, Text } = Typography;
 const ContactArea = () => {
-    const [form] = Form.useForm();
-    const onFinish = (values) => {
-        fetch('/api/send', {
-          method: 'POST',
-          headers: {
-            Accept: 'application/json, text/plain, */*',
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(values),
-        }).then((res) => {
-          form.resetFields();
-          close();
-          if (res.status === 200) {
-          }
-        });
-    };
+  const [form] = Form.useForm();
+  const onFinish = (values) => {
+    fetch('/api/send', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(values),
+    }).then((res) => {
+      form.resetFields();
+      close();
+      if (res.status === 200) {
+      }
+    });
+  };
 
-    const normFile = (e) => {
-        console.log('Upload event:', e);
-        if (Array.isArray(e)) {
-          return e;
-        }
-        return e?.fileList;
-    };
+  const normFile = (e) => {
+    console.log('Upload event:', e);
+    if (Array.isArray(e)) {
+      return e;
+    }
+    return e?.fileList;
+  };
 
   return (
     <Row>
-      <Col span={24} className="conact-area">
+      <Col span={24} className="conact-area s-blackish">
         <Row justify="center">
-          <Col sm={24} md={18} xs={24} >
-                <Button type="primary" size='large' block>Upload Your Resume</Button>
+          <Col sm={24} md={18} xs={24}>
+            <Button
+              type="primary"
+              size="large"
+              block
+              className="bgc-goldish c-white b-goldish  py-20 h-auto"
+            >
+              Upload Your Resume
+            </Button>
           </Col>
           <Col sm={24} md={18} xs={24} className="carrers-form">
             <Form
@@ -46,9 +62,8 @@ const ContactArea = () => {
               size="large"
               layout="inline"
               // onFieldsChange={validateInfo}
-              style={{width:'100%'}}
+              style={{ width: '100%' }}
               validateTrigger="onSubmit"
-              
             >
               <Row gutter={[0, 25]}>
                 <Col xs={24} sm={24} md={12}>
@@ -61,7 +76,7 @@ const ContactArea = () => {
                       },
                     ]}
                   >
-                    <Input placeholder={'Full Name:'}  />
+                    <Input placeholder={'Full Name:'} />
                   </Form.Item>
                 </Col>
                 <Col xs={24} sm={24} md={12}>
@@ -95,9 +110,7 @@ const ContactArea = () => {
                   </Form.Item>
                 </Col>
                 <Col xs={24} sm={24} md={12}>
-                  <Form.Item
-                    name="pincode"
-                  >
+                  <Form.Item name="pincode">
                     <Input placeholder={'Pin Code:'} />
                   </Form.Item>
                 </Col>
@@ -108,33 +121,41 @@ const ContactArea = () => {
                 </Col>
                 <Col span={24}>
                   <Form.Item name="Address:">
-                    <Input.TextArea autoSize={{ minRows: 3, maxRows: 3 }} placeholder="Address:" />
+                    <Input.TextArea
+                      autoSize={{ minRows: 3, maxRows: 3 }}
+                      placeholder="Address:"
+                    />
                   </Form.Item>
                 </Col>
                 <Col xs={24} sm={24} md={12}>
-                    <Form.Item
-                        name="upload"
-                        valuePropName="fileList"
-                        getValueFromEvent={normFile}
+                  <Form.Item
+                    name="upload"
+                    valuePropName="fileList"
+                    getValueFromEvent={normFile}
+                  >
+                    <Upload
+                      name="logo"
+                      action="/upload.do"
+                      listType="text"
+                      maxCount="1"
                     >
-                        <Upload name="logo" action="/upload.do" listType="text" maxCount="1">
-                            <Button icon={<UploadOutlined />}>Click to upload</Button>
-                        </Upload>
-                    </Form.Item>
+                      <Button icon={<UploadOutlined />}>Click to upload</Button>
+                    </Upload>
+                  </Form.Item>
                 </Col>
               </Row>
-              <Row className="py-30" style={{width: '100%'}} justify='center'>
+              <Row className="py-30" style={{ width: '100%' }} justify="center">
                 <Col xs={18} md={8}>
-                    <Button
+                  <Button
                     type="primary"
                     size="large"
                     shape="round"
-                    // className="conact-area-btn"
+                    className="b-goldish bgc-goldish c-white hover-s-goldish hover-c-blackish"
                     block
                     htmlType="submit"
-                    >
+                  >
                     <span>SEND NOW</span>
-                    </Button>
+                  </Button>
                 </Col>
               </Row>
             </Form>
